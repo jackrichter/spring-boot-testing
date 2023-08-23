162cdc8fc54c27064a41c5ac712475445706a060
jackrichter <jackrichter1@gmail.com>
2023-08-23 14:16:41 +0200

    JUnit test of findAll, findById and findByEmail

--- src/test/java/net/javaguides/springboot/repository/EmployeeRepositoryTests.java
+++ src/test/java/net/javaguides/springboot/repository/EmployeeRepositoryTests.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 @DataJpaTest            // Provides and configures the in-memory database (h2) for the tests
 public class EmployeeRepositoryTests {
 
@@ -31,5 +33,71 @@ public class EmployeeRepositoryTests {
         // then - verify the output
         assertThat(savedEmployee).isNotNull();
         assertThat(savedEmployee.getId()).isGreaterThan(0);
+    }
+
+    @DisplayName("JUnit test for a get all employees operation")
+    @Test
+    public void givenEmployeeList_whenFindAll_thenEmployeeList() {
+
+        // given - precondition or setup
+        Employee employee1 = Employee.builder()
+                .firstName("John")
+                .lastName("Cena")
+                .email("john@gmail.com")
+                .build();
+
+        Employee employee2 = Employee.builder()
+                .firstName("Fred")
+                .lastName("Astaire")
+                .email("fred@gmail.com").build();
+
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+
+        // when - action or behavior to be tested
+        List<Employee> list =employeeRepository.findAll();
+
+        // then - verify output
+        assertThat(list).isNotNull();
+        assertThat(list.size()).isEqualTo(2);
+    }
+
+    @DisplayName("JUnit test for a get employee by Id operation")
+    @Test
+    public void givenEmployeeObject_whenFindById_thenReturnEmployeeObject() {
+
+        // given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("John")
+                .lastName("Cena")
+                .email("john@gmail.com")
+                .build();
+        employeeRepository.save(employee);
+
+        // when - action or behavior to be tested
+        Employee savedEmployee = employeeRepository.findById(employee.getId()).get();
+
+        // then - verify output
+        assertThat(savedEmployee).isNotNull();
+    }
+
+    // JUnit test for get employee by email operation
+    @DisplayName("JUnit test for get employee by email operation")
+    @Test
+    public void givenEmployeeEmail_whenFindByEmail_thenReturnEmployeeObject() {
+
+        // given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("John")
+                .lastName("Cena")
+                .email("john@gmail.com")
+                .build();
+        employeeRepository.save(employee);
+
+        // when - action or behavior to be tested
+        Employee employeeDB = employeeRepository.findByEmail(employee.getEmail()).get();
+
+        // then - verify output
+        assertThat(employeeDB).isNotNull();
     }
 }
